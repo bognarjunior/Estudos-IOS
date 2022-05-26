1316ea2d6873cd324b7a6edc67294fd0978d1126
Bognar Junior <bognar_junior@yahoo.com.br>
2022-05-25 22:08:38 -0300

    Create table view controler

---
+++ ListaFilme/ListaFilme/TableViewController.swift
@@ -0,0 +1,102 @@
+//
+//  TableViewController.swift
+//  ListaFilme
+//
+//  Created by José Roberto Bognar Junior on 25/05/22.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var movies = ["O Senhor dos Anéis: A Sociedade do Anel",
+                  "Senhor dos Anéis: As Duas Torres",
+                  "Senhor dos Anéis: O Retorno do Rei",
+                  "Star Wars: A Nova Esperança",
+                  "Star Wars: O Império Contra-ataca",
+                  "Star Wars: O Retorno de Jedi",
+                  "Star Wars: A Ameaça Fantasma",
+                  "Star Wars: Ataque dos Clones",
+                  "Star Wars: A Vingança dos Sith",
+                  "Star Wars: O Despertar da Força",
+                  "Star Wars: Os Últimos Jedi",
+                  "Star Wars: A Ascensão Skywalker"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return movies.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listMovies", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = movies[indexPath.row]
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
