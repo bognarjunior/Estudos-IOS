e4576e2b4aacc16aee99200b87ece908bb969877
Bognar Junior <bognar_junior@yahoo.com.br>
2022-05-27 14:14:41 -0300

    Estudo básico tipos

---
+++ EstudoIOSPlayground/EstudoIOSPlayGround.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var message = "Olá Mundo!"
+
+print(message)
+
+let number: Int = 32
+let PI: Double = 3.14
+let name: String = "Bognar Junior"
+var isDriver: Bool = true
+
+print(number)
+print(PI)
+print(name)
+print(isDriver)
+
