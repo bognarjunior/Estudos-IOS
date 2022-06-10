139d1e9ceea54ab1f1e97ff5c6123ff6e1affe8e
Bognar Junior <bognar_junior@yahoo.com.br>
2022-06-09 21:08:16 -0300

    Calculate age of dog
    Calculate age of dog

--- AgeDog/AgeDog/ViewController.swift
+++ AgeDog/AgeDog/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var textAge: UITextField!
+    
+    @IBAction func discoverAge(_ sender: Any) {
+        
+        if textAge.text == "" {
+            resultLabel.text = "Digite um número na caixa de texto"
+        } else {
+            let result:Int = Int(textAge.text!)! * 7
+            
+            resultLabel.text = "A idade do cachorro em anos humanos é: \(result)"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        resultLabel.text = ""
     }
 
 
