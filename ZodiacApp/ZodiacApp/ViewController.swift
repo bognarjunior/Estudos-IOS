f581d424cf4ccc31eb0c8ae404ed5ffb31fbe7da
Bognar Junior <bognar_junior@yahoo.com.br>
2022-06-18 21:25:59 -0300

    Create Zodiac App
    Create Zodiac App

---
+++ ZodiacApp/ZodiacApp/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  ZodiacApp
+//
+//  Created by José Roberto Bognar Junior on 18/06/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var cellId = "cellId"
+    var sings:[String] = [
+        "Áries",
+        "Touro",
+        "Gêmeos",
+        "Câncer",
+        "Leão",
+        "Virgem",
+        "Libra",
+        "Escorpião",
+        "Sagitário",
+        "Capricórnio",
+        "Aquário",
+        "Peixes"
+    ]
+    var meanings:[String] = [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis est in dui luctus vestibulum. Curabitur mattis, sem sit amet.",
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.sings.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = self.sings[indexPath.row]
+        return cell
+    }
+}
+
