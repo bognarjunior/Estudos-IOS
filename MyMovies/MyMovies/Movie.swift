cff0b576f52017e786bed9307b17e45dcb9523a3
Bognar Junior <bognar_junior@yahoo.com.br>
2022-06-20 21:44:00 -0300

    Create class Movie
    Create class Movie and add functions in ViewController

---
+++ MyMovies/MyMovies/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  MyMovies
+//
+//  Created by José Roberto Bognar Junior on 20/06/22.
+//
+
+import UIKit
+
+class Movie {
+    var title: String!
+    var description: String!
+    var image: UIImage!
+    
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+}
--- MyMovies/MyMovies/ViewController.swift
+++ MyMovies/MyMovies/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    let cellId = "cellId"
+    var movies: [Movie] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var movie: Movie
+        movie = Movie(title: "Filme 1", description: "Descrição 1")
+        movies.append(movie)
+        movie = Movie(title: "Filme 2", description: "Descrição 2")
+        movies.append(movie)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.movies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let movie = self.movies[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.textLabel?.text = movie.title
+        return cell
+    }
 }
 
