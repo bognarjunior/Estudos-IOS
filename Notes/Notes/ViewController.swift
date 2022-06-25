47704ab2ed1bc789b205a76d772a3a700de0ccb4
Bognar Junior <bognar_junior@yahoo.com.br>
2022-06-25 15:42:28 -0300

    Create functions
    Create functions save and get notes

--- Notes/Notes/ViewController.swift
+++ Notes/Notes/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let keyNote: String = "note"
+    @IBOutlet weak var txtNotes: UITextView!
+    
+    @IBAction func saveNotes(_ sender: Any) {
+        if let text = txtNotes.text {
+            UserDefaults.standard.set(text, forKey: keyNote)
+        }
+    }
+    
+    func getNote() -> String {
+        if let recoveredText = UserDefaults.standard.object(forKey: keyNote){
+            return recoveredText as! String
+        }
+        return ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        txtNotes.text = getNote()
     }
 
 
