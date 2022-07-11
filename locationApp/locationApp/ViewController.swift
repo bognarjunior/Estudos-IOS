10e914d0c526442ef9d27ba9f4a22a7cc61ef78c
Bognar Junior <bognar_junior@yahoo.com.br>
2022-07-10 21:15:04 -0300

    Requesting location and displaying settings
    Requesting location and displaying settings

--- locationApp/locationApp/ViewController.swift
+++ locationApp/locationApp/ViewController.swift
@@ -8,13 +8,44 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
-    
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let accuracyAuthorization = manager.accuracyAuthorization
+        switch accuracyAuthorization {
+        case .fullAccuracy:
+            break
+        case .reducedAccuracy:
+            self.configAlert()
+            break
+        default:
+            break
+        }
+    }
+    
+    func configAlert() {
+        let alertController = UIAlertController(title: "Permissão de localização", message: "É necessária a permissão de localização para o app funcionar", preferredStyle: .alert)
+        let configAction = UIAlertAction(title: "Abrir configurações", style: .default, handler: { (alertConfig) in
+            if let config = NSURL(string: UIApplicationOpenNotificationSettingsURLString) {
+                UIApplication.shared.open(config as URL)
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+        alertController.addAction(configAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
     }
 
 
