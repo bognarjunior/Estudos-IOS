62e8e23281b4f56a10d6090094dbae2a4138f025
Bognar Junior <bognar_junior@yahoo.com.br>
2022-07-13 22:01:59 -0300

    Create app MyTravels
    Create app MyTravels

---
+++ MyTravels/MyTravels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTravels
+//
+//  Created by José Roberto Bognar Junior on 13/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
