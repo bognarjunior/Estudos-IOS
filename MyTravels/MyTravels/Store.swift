0cfbb264ef11eec7a9729099aa3f067187c6eefc
Bognar Junior <bognar_junior@yahoo.com.br>
2022-07-19 21:47:00 -0300

    Create Storage
    Create Storage

---
+++ MyTravels/MyTravels/Store.swift
@@ -0,0 +1,23 @@
+//
+//  Store.swift
+//  MyTravels
+//
+//  Created by José Roberto Bognar Junior on 19/07/22.
+//
+
+import UIKit
+
+class Store{
+    let KEY = "MYTRAVELS"
+    func addTravel() {
+        
+    }
+    
+    func getTravels() {
+        
+    }
+    
+    func removeTravel() {
+        
+    }
+}
